345a5803e232345ee40e4237e10bb5d40c3cd9ef
devboy2011 <buituananhit.contact@gmail.com>
2023-09-08 12:03:31 +0700

    Login, verify token with user request

---
+++ server.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import Jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+//require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware to recieve json data submitted from client
+app.use(express.json());
+
+const books = [
+    {
+        id : 1,
+        name : 'Chi Pheo',
+        author : 'Nam Cao'
+    },
+    {
+        id : 2,
+        name : 'Chien tranh va Hoa binh',
+        author : 'Victor Hugo'
+    }
+]
+
+app.get('/books', authenToken, (req,res) => {
+    res.json({ status : "Success", data : books});
+})
+
+function authenToken (req, res, next) {
+    const authorizationHeader = req.headers['authorization'];
+    
+    const token = authorizationHeader.split(' ')[1];
+    
+    if (!token) res.sendStatus(401);
+    
+    Jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, data) => {
+       console.log(error, data);
+       // Forbidden code
+       if (error) res.sendStatus(403);
+       next();
+    });
+}
+
+app.post('/login',  (req, res) => {
+    /*
+        Authentication
+    */
+    
+    // Authorization
+    const data = req.body;
+    
+    const accessToken = Jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, { 
+        expiresIn : '10s'
+    });
+    
+    res.json({accessToken});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+})
